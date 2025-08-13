1c462d4c2029f1f5601201d0b94851ea42548592
SagarJ95 <sagarjagade2023@gmail.com>
2025-08-13 18:17:36 +0530

    index.css file import remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import Layout from './components/layout/layout'
+import Login from './components/pages/login'
+import { Routes,Route } from 'react-router'
+import { useLocation } from 'react-router'
+import Dashboard from './components/pages/dashboard'
 function App() {
-
+  const location = useLocation()
+  const adminLogin = location.pathname === '/admin'
   return (
     <>
-    <Layout>
-    <p>New result</p>
-    </Layout>
-    </>
+      <Routes>
+      <Route path="/admin" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
+      </Routes>
+        </>
   )
 }
 
--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,8 +2,9 @@
 import Headers from "./header";
 import Footer from "./footer";
 import Siderbar from "./sidebar";
+import { Outlet } from "react-router";
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <div className="d-flex flex-column flex-root app-root" id="kt_app_root">
       <div className="app-page flex-column flex-column-fluid" id="kt_app_page">
@@ -12,7 +13,7 @@ const Layout = ({ children }) => {
           <Siderbar />
           <div className="app-main flex-column flex-row-fluid" id="kt_app_main">
             <div className="d-flex flex-column flex-column-fluid">
-              {children} {/* This replaces your <body tag/> */}
+              <Outlet/>
             </div>
             <Footer />
           </div>
